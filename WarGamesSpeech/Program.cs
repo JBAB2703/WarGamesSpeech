753e3c8ef5b06dc6ed4487e68b915f60090d698b
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-23 11:21:30 -0400

    Add project files.

---
+++ WarGamesSpeech/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGamesSpeech {
+    class Program {
+        static void Main(string[] args) {
+
+            var speach = new System.Speech.Synthesis.SpeechSynthesizer();
+            speach.Speak("You mean to tell me!");
+
+
+        }
+    }
+}
